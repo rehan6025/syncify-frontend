c021551ebd688fc3908d8791d84d96422760e692
rehan6025 <126170930+rehan6025@users.noreply.github.com>
2025-05-07 23:16:26 +0530

    Update ConnectButton.jsx

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -7,7 +7,7 @@ function ConnectButton({service}) {
     }
 
   return (
-    <button onClick={handleConnect} className="bg-green-500 px-4 py-2 rounded" >
+    <button onClick={handleConnect} className="bg-transparent px-4 py-2 rounded cursor-pointer hover:bg-purple-400 transition-colors border-2 border-purple-300 hover:text-yellow-200 duration-300  " >
         Connect {service}
     </button>
   )
