93cb806f112d61ac5700970cb9ac6073385d1cae
Rehan Ahmed <126170930+rehan6025@users.noreply.github.com>
2025-08-17 13:44:49 +0530

    Update TransferPlaylistPage.jsx

--- src/pages/TransferPlaylistPage.jsx
+++ src/pages/TransferPlaylistPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Music, Youtube, ArrowRight, Loader } from 'lucide-react'
 
 //using fetches to transfer playlists
 function TransferPlaylistPage() {
